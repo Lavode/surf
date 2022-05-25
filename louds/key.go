3beac3510cedebd921efa0315cf5cdde1fc17282
Michael Senn <michael@morrolan.ch>
2022-05-25 20:00:15 +0200

    Implement key truncation.

--- louds/key.go
+++ louds/key.go
@@ -36,3 +36,103 @@ func (key Key) Less(other Key) bool {
 	// is not the lesser).
 	return false
 }
+
+// Truncate truncates the list of keys such that they are still uniquely
+// identifiable.
+//
+// The inputs must be sorted and may not contain any duplicates. Then, each key
+// is truncated to as short a prefix as possible for them to still be
+// unique.
+//
+// As an example, the following keys:
+// - far
+// - fast
+// - john
+// Would be truncated to:
+// - far
+// - fas
+// - j
+func Truncate(keys []Key) []Key {
+	out := make([]Key, len(keys))
+
+	for i, key := range keys {
+		// To be able to truncate a key we must find the lowest-indexed
+		// bytes where:
+		// - It differs from the equivalent byte of the preceeding key
+		// - It differs from the equivalent byte of the next key
+		//
+		// Then, the larger of the two defines the boundary of a prefix
+		// of the key such that it can be distinguished from both the
+		// key before as well as the one after.
+
+		var firstDifferenceBefore, firstDifferenceAfter int
+		var differ bool
+
+		if i != 0 {
+			differ, firstDifferenceBefore = FirstDifferenceAt(key, keys[i-1])
+			if !differ {
+				firstDifferenceBefore = len(key)
+			}
+		}
+
+		if i != len(keys)-1 {
+			differ, firstDifferenceAfter = FirstDifferenceAt(key, keys[i+1])
+			if !differ {
+				firstDifferenceAfter = len(key)
+			}
+		}
+
+		var n int
+		if firstDifferenceAfter > firstDifferenceBefore {
+			n = firstDifferenceAfter
+		} else {
+			n = firstDifferenceBefore
+		}
+
+		if n < len(key) {
+			// We have the lowest index such that the prefix differs, so we
+			// must include this one
+			// However this would be invalid if the current key had
+			// be the shorter of the two, as then the first index
+			// where the two differ willl already be equal to
+			// len(key).
+			n++
+		}
+
+		out[i] = key[:n]
+	}
+
+	return out
+}
+
+// FirstDifferenceAt compares two byte slices, finding the first byte where the
+// two differ.
+//
+// The first returned argument indicates whether the two differ. If it is true,
+// the second indicates the first index at which the two differ.
+//
+// If the two are of different length, with the shorter being a prefix of the
+// longer, then the first byte of the longer is the one which is considered to
+// differ.
+func FirstDifferenceAt(a, b []byte) (bool, int) {
+	var n int
+	if len(a) < len(b) {
+		n = len(a)
+	} else {
+		n = len(b)
+	}
+
+	for i := 0; i < n; i++ {
+		if a[i] != b[i] {
+			return true, i
+		}
+	}
+
+	// Either the two are equal, or one is longer, in which case the first
+	// difference is the first byte of the longer of the two.
+	if len(a) == len(b) {
+		return false, 0
+	} else {
+		return true, n
+	}
+}
--- louds/key_test.go
+++ louds/key_test.go
@@ -1,9 +1,13 @@
 package louds
 
 import (
+	"bytes"
+	"log"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"golang.org/x/exp/slices"
 )
 
 func TestLess(t *testing.T) {
@@ -26,4 +30,105 @@ func TestLess(t *testing.T) {
 	// Transitivity
 	assert.True(t, a.Less(c))
 	assert.False(t, c.Less(a))
+}
+
+func TestTruncate(t *testing.T) {
+	keys := []Key{
+		[]byte("f"),
+		[]byte("farther"),
+		[]byte("fas"),
+		[]byte("fasten"),
+		[]byte("fat"),
+		[]byte("splice"),
+		[]byte("topper"),
+		[]byte("toy"),
+		[]byte("tries"),
+		[]byte("tripper"),
+		[]byte("trying"),
+	}
+
+	expected := []Key{
+		[]byte("f"),
+		[]byte("far"),
+		[]byte("fas"),
+		[]byte("fast"),
+		[]byte("fat"),
+		[]byte("s"),
+		[]byte("top"),
+		[]byte("toy"),
+		[]byte("trie"),
+		[]byte("trip"),
+		[]byte("try"),
+	}
+
+	truncated := Truncate(keys)
+	assert.Equal(t, expected, truncated)
+}
+
+func TestTruncateRandom(t *testing.T) {
+	rand.Seed(42)
+
+	numKeys := 1_000_000
+	maxKeyLength := 128
+
+	// Generate set of random keys
+	keys := make([]Key, numKeys)
+	for i := 0; i < numKeys; i++ {
+		key := make([]byte, rand.Intn(maxKeyLength)+1)
+		rand.Read(key)
+		keys[i] = key
+	}
+
+	sort := func(x, y Key) bool {
+		return x.Less(y)
+	}
+	slices.SortFunc(keys, sort)
+
+	log.Printf("Before compaction: %d", len(keys))
+	// We can't have any duplicates to start with
+	equal := func(x, y Key) bool {
+		return bytes.Equal(x, y)
+	}
+	keys = slices.CompactFunc(keys, equal)
+	log.Printf("After compaction: %d", len(keys))
+
+	truncated := Truncate(keys)
+	for i := 0; i < len(truncated)-1; i++ {
+		// Keys' prefixes must be preserved
+		assert.True(t, bytes.HasPrefix(keys[i], truncated[i]))
+
+		// Keys must still be unique
+		assert.NotEqual(t, truncated[i], truncated[i+1])
+	}
+}
+
+func TestFirstDifferenceAt(t *testing.T) {
+	a := []byte{0x00, 0x01}
+	b := []byte{0x01, 0x00}
+	differs, idx := FirstDifferenceAt(a, b)
+	assert.True(t, differs)
+	assert.Equal(t, 0, idx)
+
+	a = []byte{0x00, 0x01, 0x02, 0x00}
+	b = []byte{0x00, 0x01, 0x03, 0x00}
+	differs, idx = FirstDifferenceAt(a, b)
+	assert.True(t, differs)
+	assert.Equal(t, 2, idx)
+
+	a = []byte{0x00, 0x01, 0x02}
+	b = []byte{0x00, 0x01, 0x02, 0x03}
+	differs, idx = FirstDifferenceAt(a, b)
+	assert.True(t, differs)
+	assert.Equal(t, 3, idx)
+
+	a = []byte{0x00, 0x01, 0x02, 0x03}
+	b = []byte{0x00, 0x01, 0x02}
+	differs, idx = FirstDifferenceAt(a, b)
+	assert.True(t, differs)
+	assert.Equal(t, 3, idx)
+
+	a = []byte{0x00, 0x01, 0x02, 0x03}
+	b = []byte{0x00, 0x01, 0x02, 0x03}
+	differs, idx = FirstDifferenceAt(a, b)
+	assert.False(t, differs)
 }
