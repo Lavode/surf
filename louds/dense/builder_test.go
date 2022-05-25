ab5d3cecacc102ca87540837a595deed65d7e31c
Michael Senn <michael@morrolan.ch>
2022-05-25 13:13:55 +0200

    Expand test LOUDS-DENSE test suite.

--- louds/dense/builder_test.go
+++ louds/dense/builder_test.go
@@ -65,25 +65,127 @@ func TestBuildTwoLevels(t *testing.T) {
 	expectedHasChild := bitmap.New(768, 768)
 	expectedIsPrefixKey := bitmap.New(3, 256)
 
-	// First node: Edges f, a
-	k := 0
-	expectedLabels.Set(k*256 + 102) // f
-	expectedLabels.Set(k*256 + 97)  // a
-	// And both have FST sub-trees
-	expectedHasChild.Set(k*256 + 102) // f
-	expectedHasChild.Set(k*256 + 97)  // a
+	labels := []int{
+		// First node: Edges a, f
+		0*256 + 'a',
+		0*256 + 'f',
+		// Second node: Edges i, o
+		1*256 + 'i',
+		1*256 + 'o',
+		// Third node: Edges a, e
+		2*256 + 'a',
+		2*256 + 'e',
+	}
+	for _, bit := range labels {
+		assert.Nil(t, expectedLabels.Set(bit))
+	}
 
-	// Second node: Edges i, o
-	k++
-	expectedLabels.Set(k*256 + 105) // i
-	expectedLabels.Set(k*256 + 111) // o
+	children := []int{
+		// First node: a, f have sub-tree
+		0*256 + 'a',
+		0*256 + 'f',
+	}
+	for _, bit := range children {
+		assert.Nil(t, expectedHasChild.Set(bit))
+	}
 
-	// Third node: Edges a, e
-	k++
-	expectedLabels.Set(k*256 + 97)  // a
-	expectedLabels.Set(k*256 + 101) // e
-	// And 'f' is prefix key
-	expectedIsPrefixKey.Set(k)
+	prefixKeys := []int{
+		// Third node (-> 'f'): Is prefix key
+		2,
+	}
+	for _, bit := range prefixKeys {
+		assert.Nil(t, expectedIsPrefixKey.Set(bit))
+	}
+
+	// Let's test it :)
+	assert.Nil(t, builder.Build(keys))
+
+	assert.True(t, expectedLabels.Equal(builder.Labels), "Expected Labels:\n%s\nGot:\n%s", expectedLabels, builder.Labels)
+	assert.True(t, expectedHasChild.Equal(builder.HasChild), "Expected HasChild:\n%s\nGot:\n%s", expectedHasChild, builder.HasChild)
+	assert.True(t, expectedIsPrefixKey.Equal(builder.IsPrefixKey), "Expected IsPrefixKey:\n%s\nGot:\n%s", expectedIsPrefixKey, builder.IsPrefixKey)
+}
+
+func TestBuildMultiLevel(t *testing.T) {
+	builder := NewBuilder(BUILDER_MEMORY_LIMIT)
+
+	keys := []louds.Key{
+		[]byte("f"),
+		[]byte("far"),
+		[]byte("fas"),
+		[]byte("fast"),
+		[]byte("fat"),
+		[]byte("s"),
+		[]byte("top"),
+		[]byte("toy"),
+		[]byte("trie"),
+		[]byte("trip"),
+		[]byte("try"),
+	}
+
+	expectedLabels := bitmap.New(8*256, 8*256)
+	expectedHasChild := bitmap.New(8*256, 8*256)
+	expectedIsPrefixKey := bitmap.New(8, 256)
+
+	labels := []int{
+		// First node: Edges f, s, t
+		0*256 + 'f',
+		0*256 + 's',
+		0*256 + 't',
+		// Second node: Edges a
+		1*256 + 'a',
+		// Third node: Edges o, r
+		2*256 + 'o',
+		2*256 + 'r',
+		// Fourth node: Edges r, s, t
+		3*256 + 'r',
+		3*256 + 's',
+		3*256 + 't',
+		// Fifth node: Edges p, y
+		4*256 + 'p',
+		4*256 + 'y',
+		// Sixth node: Edges i, y
+		5*256 + 'i',
+		5*256 + 'y',
+		// Seventh node: Edges t
+		6*256 + 't',
+		// Eight node: Edges e, p
+		7*256 + 'e',
+		7*256 + 'p',
+	}
+	for _, bit := range labels {
+		assert.Nil(t, expectedLabels.Set(bit))
+	}
+
+	children := []int{
+		// First node: f, t have sub-tree
+		0*256 + 'f',
+		0*256 + 't',
+		// Second node: a has sub-tree
+		1*256 + 'a',
+		// Third node: o, r have sub-tree
+		2*256 + 'o',
+		2*256 + 'r',
+		// Fourth node: s has sub-tree
+		3*256 + 's',
+		// Fifth node: No sub-trees
+		// Sixth node: i has sub-tree
+		5*256 + 'i',
+		// Seventh node: No sub-trees
+		// Eight node: No sub-trees
+	}
+	for _, bit := range children {
+		assert.Nil(t, expectedHasChild.Set(bit))
+	}
+
+	prefixKeys := []int{
+		// Second node (-> 'f'): Is prefix key
+		1,
+		// Seventh node (-> 'fas'): Is prefix key
+		6,
+	}
+	for _, bit := range prefixKeys {
+		assert.Nil(t, expectedIsPrefixKey.Set(bit))
+	}
 
 	// Let's test it :)
 	assert.Nil(t, builder.Build(keys))
@@ -92,4 +194,5 @@ func TestBuildTwoLevels(t *testing.T) {
 	assert.True(t, expectedHasChild.Equal(builder.HasChild), "Expected HasChild:\n%s\nGot:\n%s", expectedHasChild, builder.HasChild)
 	assert.True(t, expectedIsPrefixKey.Equal(builder.IsPrefixKey), "Expected IsPrefixKey:\n%s\nGot:\n%s", expectedIsPrefixKey, builder.IsPrefixKey)
 
+	_ = keys
 }
