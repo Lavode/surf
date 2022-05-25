c9faec57cecc93ff086871255df0e2a6510be628
Michael Senn <michael@morrolan.ch>
2022-05-25 11:28:29 +0200

    Further work on louds-dense builder.

--- louds/dense/builder.go
+++ louds/dense/builder.go
@@ -2,37 +2,44 @@ package dense
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Lavode/surf/bitmap"
 	"github.com/Lavode/surf/louds"
 )
+
+// NodeTask contains things which need to be considered for building up one
+// future node.
+//
+// This includes keys whose path contains that node, but also additional
+// information such as that the node might be a prefix key.
+type NodeTask struct {
+	// keys is the slice of keys which will lead to edges on the future node.
+	keys []louds.Key
+	// isPrefixKey defines whether this node's isPrefixKey flag will have
+	// to be set to true - if the node will exist at all.
+	isPrefixKey bool
+}
 
 type Builder struct {
 	Labels      *bitmap.Bitmap
 	HasChild    *bitmap.Bitmap
 	IsPrefixKey *bitmap.Bitmap
 
-	// keys contains the keys which the builder will use to build up an FST
-	// tree.
-	keys []louds.Key
+	// tasks is a slice of tasks to be taken care of to define nodes
+	// further down the tree.
+	// There is a 1:1 correspondence between tasks and (potential) future
+	// nodes.
+	tasks []*NodeTask
+	// currentTask is a pointer to the most recent element of the tasks
+	// slice.
+	// As such it is not the task currently being worked on, but the task
+	// currently being defined. A bit of a misnormer I admit.
+	currentTask *NodeTask
 
-	// groups contains the keys, grouped by which node of the current level
-	// they belong to.
-	// For currentLevel = 0, it will contain one group equal to keys.
-	// For currentLevel = 1, it will contain the keys grouped by their
-	// first byte, and so on.
-	groups [][]louds.Key
-
-	// Node ID in level-order of node we are currently building up.
+	// currentNodeId is the 0-indexed level-order ID of the node we are
+	// currently building up.
 	currentNodeId int
-	// currentLevel specifies the current level of the tree we are working
-	// on.
-	currentLevel int
-	// currentEdge specifies the most recently added edge of the current node.
-	currentEdge byte
-	// currentNodeHasEdges specifies whether the current node has had any
-	// edges added
-	currentNodeHasEdges bool
 }
 
 // NewBuilder instantiates a new LOUDS-DENSE builder.
@@ -47,7 +54,7 @@ func NewBuilder(memory_limit int) *Builder {
 		Labels:      bitmap.New(256, 256*memory_unit),
 		HasChild:    bitmap.New(256, 256*memory_unit),
 		IsPrefixKey: bitmap.New(1, memory_unit),
-		groups:      make([][]louds.Key, 0),
+		tasks:       make([]*NodeTask, 0),
 	}
 
 	return &builder
@@ -64,50 +71,190 @@ func (builder *Builder) NodeCapacity() int {
 // Build may only be called on a freshly created instance. Calling Build on a
 // builder more than once is not guaranteed to produce a consistent tree.
 func (builder *Builder) Build(keys []louds.Key) error {
-	builder.keys = keys
+	// For depth = 0 we'll consider all keys
+	builder.appendNodeTask()
+	builder.currentTask.keys = keys
 
-	for _, key := range builder.keys {
-		fmt.Printf("Setting key: %x\n", key)
-		err := builder.addEdge(key[0])
-		if err != nil {
-			return err
+	for depth := 0; depth < maxKeyLength(keys); depth++ {
+		log.Printf("Current depth: %d", depth)
+
+		// During interation we'll be adding tasks of the next tree
+		// level. But we only want to consider tasks of the current
+		// level.
+		n := len(builder.tasks)
+		for i := 0; i < n; i++ {
+			task := builder.tasks[i]
+
+			if len(task.keys) == 0 {
+				log.Printf("\tSkipping empty task")
+				continue
+			}
+
+			log.Printf("\tConsidering pending task with keys %s, isPrefixKey %t (now in node %d)", task.keys, task.isPrefixKey, builder.currentNodeId)
+			// Each task corresponds to one node, so us starting
+			// with a new task means we're populating a new node
+			nodeHasEdges := false
+			var mostRecentEdge byte = 0x00
+
+			err := builder.initializeNode()
+			if err != nil {
+				return err
+			}
+
+			// If the node is non-empty (which is the case if we are here), and the task has
+			// its isPrefixKey flag set, then that means that one key ended on this node.
+			if task.isPrefixKey {
+				log.Printf("\t\tSetting node's is-prefix-key flag")
+				builder.setIsPrefixKey()
+			}
+
+			for _, key := range task.keys {
+				edge := key[depth]
+				log.Printf("\t\tConsidering key %s (edge = %c)", key, edge)
+
+				if !nodeHasEdges || mostRecentEdge != edge {
+					log.Printf("\t\t\tDefining new edge with value %c", edge)
+
+					err := builder.addEdge(edge)
+					if err != nil {
+						return err
+					}
+
+					// Having added a new edge means that there will also, on the next level,
+					// be a new node which future keys (if we're not at their end yet) will go into.
+					builder.appendNodeTask()
+
+					mostRecentEdge = edge
+					nodeHasEdges = true
+				}
+
+				if depth == len(key)-1 {
+					log.Printf("\t\t\tReached end of key")
+
+					// Key ends at the node next to its edge, so if that node exists it will
+					// have to have IsPrefixKey set to true
+					builder.currentTask.isPrefixKey = true
+				} else {
+					err := builder.setHasChild(edge)
+					if err != nil {
+						return nil
+					}
+
+					log.Printf("\t\t\tKey must be considered further, adding to tasks of next level")
+					builder.currentTask.keys = append(builder.currentTask.keys, key)
+				}
+
+			}
+
+			// Reached end of the current node.
+			builder.currentNodeId++
 		}
-	}
 
+		// We processed all tasks of the current level, so we'll
+		// discard them.
+		builder.tasks = builder.tasks[n:]
+	}
 	return nil
 }
 
-func (builder *Builder) addEdge(label byte) error {
-	// We must not skip any keys if we haven't actually added any edges
-	// yet, as in this case currentEdge's default value will be 0x00 which
-	// might be equal to the key.
-	if builder.currentNodeHasEdges && builder.currentEdge == label {
-		return nil
-	}
-
-	bit := builder.labelsOffset() + int(label)
-	fmt.Printf("Added edge for key = %x at bitmap offset = %x\n", label, bit)
+func (builder *Builder) addEdge(edge byte) error {
+	bit := builder.labelOffset() + int(edge)
 	err := builder.Labels.Set(bit)
-	if err != nil {
-		return fmt.Errorf("DENSE builder: Error setting label: %v", err)
-	}
 
-	builder.currentEdge = label
+	if err != nil {
+		return fmt.Errorf(
+			"LOUDS-Dense builder: Error setting bit for edge %x at %d: %v",
+			edge,
+			bit,
+			err,
+		)
+	}
 
 	return nil
 }
 
-func (builder *Builder) beginNode() {
-}
+func (builder *Builder) setHasChild(edge byte) error {
+	bit := builder.hasChildOffset() + int(edge)
+	err := builder.HasChild.Set(bit)
 
-func (builder *Builder) setLabel(nodeId int, label byte) error {
-	offset := 256*nodeId + int(label)
-	err := builder.Labels.Set(offset)
 	if err != nil {
-		return fmt.Errorf("DENSE Builder: Error setting Label: %v", err)
+		return fmt.Errorf(
+			"LOUDS-Dense builder: Error setting bit for has-child of edge %x at %d: %v",
+			edge,
+			bit,
+			err,
+		)
 	}
 
 	return nil
+}
+
+func (builder *Builder) setIsPrefixKey() error {
+	bit := builder.isPrefixKeyOffset()
+	err := builder.IsPrefixKey.Set(bit)
+
+	if err != nil {
+		return fmt.Errorf(
+			"LOUDS-Dense builder: Error setting bit for is-prefix-key at %d: %v",
+			bit,
+			err,
+		)
+	}
+
+	return nil
+}
+
+// labelOffset returns the offset in the D-Labels bitmap of the currently
+// processed node.
+func (builder *Builder) labelOffset() int {
+	return builder.currentNodeId * 256
+}
+
+// hasChildOffset returns the offset in the D-HasChild bitmap of the currently
+// processed node.
+func (builder *Builder) hasChildOffset() int {
+	return builder.currentNodeId * 256
+}
+
+// isPrefixKeyOffset returns the offset in the D-IsPrefixKey bitmap of the
+// currently processed node.
+func (builder *Builder) isPrefixKeyOffset() int {
+	return builder.currentNodeId
+}
+
+// initialzeNode initialies the various bitmaps to accomodate the new node with
+// ID builder.currentNodeId.
+func (builder *Builder) initializeNode() error {
+	// We'll make sure the current node's extents in the various bitmaps is
+	// allocated.
+	// This is not strictly needed, but makes for cleaner / easier to test
+	// results.
+
+	_, err := builder.Labels.Get(builder.labelOffset() + 255)
+	if err != nil {
+		return fmt.Errorf("LOUDS-Dense builder: Error initializing labels bitmap: %v", err)
+	}
+
+	_, err = builder.HasChild.Get(builder.hasChildOffset() + 255)
+	if err != nil {
+		return fmt.Errorf("LOUDS-Dense builder: Error initializing has-child bitmap: %v", err)
+	}
+
+	_, err = builder.IsPrefixKey.Get(builder.isPrefixKeyOffset())
+	if err != nil {
+		return fmt.Errorf("LOUDS-Dense builder: Error initializing is-prefix-key bitmap: %v", err)
+	}
+
+	return nil
+}
+
+func (builder *Builder) appendNodeTask() {
+	task := &NodeTask{
+		keys: make([]louds.Key, 0),
+	}
+
+	builder.tasks = append(builder.tasks, task)
+	builder.currentTask = task
 }
 
 func maxKeyLength(keys []louds.Key) int {
@@ -119,22 +266,4 @@ func maxKeyLength(keys []louds.Key) int {
 	}
 
 	return maxKeyLength
-}
-
-// labelsOffset returns the offset where the current node starts in the Labels
-// bitmap.
-func (builder *Builder) labelsOffset() int {
-	return 256 * builder.currentNodeId
-}
-
-// hasChildOffset returns the offset where the current node starts in the
-// HasChild bitmap.
-func (builder *Builder) hasChildOffset() int {
-	return 256 * builder.currentNodeId
-}
-
-// isPrefixKeyOffset returns the offset where the current node starts in the
-// IsPreixKey bitmap.
-func (builder *Builder) isPrefixKeyOffset() int {
-	return builder.currentNodeId
 }
--- louds/dense/builder_test.go
+++ louds/dense/builder_test.go
@@ -37,48 +37,6 @@ var keys []louds.Key = []louds.Key{
 	[]byte("try"),  // 10
 }
 
-func TestAddEdge(t *testing.T) {
-	b := NewBuilder(BUILDER_MEMORY_LIMIT)
-
-	// As our first edge we'll use what is the default value of
-	// builder.currentEdge, to make sure this is handled properly.
-	// A new edge should:
-	assert.Nil(t, b.addEdge(0x00))
-	// - Be kept track of
-	assert.Equal(t, uint8(0x00), b.currentEdge)
-	// - Be added to the bitmap
-	val, err := b.Labels.Get(0x00)
-	assert.Nil(t, err)
-	assert.Equal(t, uint8(1), val)
-
-	// An existing edge should:
-	assert.Nil(t, b.addEdge(0x42))
-	// - Not change the current edge
-	assert.Equal(t, uint8(0x42), b.currentEdge)
-	// - Not affect the bitmap
-	val, err = b.Labels.Get(0x42)
-	assert.Nil(t, err)
-	assert.Equal(t, uint8(1), val)
-
-	// A second new edge for flavour
-	assert.Nil(t, b.addEdge(0xF7))
-	// - Be kept track of
-	assert.Equal(t, uint8(0xF7), b.currentEdge)
-	// - Be added to the bitmap
-	val, err = b.Labels.Get(0xF7)
-	assert.Nil(t, err)
-	assert.Equal(t, uint8(1), val)
-}
-
-func TestBeginNode(t *testing.T) {
-	b := NewBuilder(BUILDER_MEMORY_LIMIT)
-
-	// Starting a new node should:
-	b.beginNode()
-	// - Mark that the current node has no edges defined yet
-	assert.Equal(t, false, b.currentNodeHasEdges)
-}
-
 func TestBuildOneLevel(t *testing.T) {
 	builder := NewBuilder(BUILDER_MEMORY_LIMIT)
 
@@ -91,10 +49,47 @@ func TestBuildOneLevel(t *testing.T) {
 	for _, k := range keys {
 		assert.Nil(t, expectedLabels.Set(int(k[0])))
 	}
-	assert.Nil(t, expectedIsPrefixKey.Set(0))
 
 	assert.Nil(t, builder.Build(keys))
 
 	assert.True(t, expectedLabels.Equal(builder.Labels), "Expected Labels:\n%s\nGot:\n%s", expectedLabels, builder.Labels)
 	assert.True(t, expectedHasChild.Equal(builder.HasChild), "Expected HasChild:\n%s\nGot:\n%s", expectedHasChild, builder.HasChild)
+	assert.True(t, expectedIsPrefixKey.Equal(builder.IsPrefixKey), "Expected IsPrefixKey:\n%s\nGot:\n%s", expectedIsPrefixKey, builder.IsPrefixKey)
+}
+
+func TestBuildTwoLevels(t *testing.T) {
+	builder := NewBuilder(BUILDER_MEMORY_LIMIT)
+	keys := []louds.Key{[]byte("ai"), []byte("ao"), []byte("f"), []byte("fa"), []byte("fe")}
+
+	expectedLabels := bitmap.New(768, 768)
+	expectedHasChild := bitmap.New(768, 768)
+	expectedIsPrefixKey := bitmap.New(3, 256)
+
+	// First node: Edges f, a
+	k := 0
+	expectedLabels.Set(k*256 + 102) // f
+	expectedLabels.Set(k*256 + 97)  // a
+	// And both have FST sub-trees
+	expectedHasChild.Set(k*256 + 102) // f
+	expectedHasChild.Set(k*256 + 97)  // a
+
+	// Second node: Edges i, o
+	k++
+	expectedLabels.Set(k*256 + 105) // i
+	expectedLabels.Set(k*256 + 111) // o
+
+	// Third node: Edges a, e
+	k++
+	expectedLabels.Set(k*256 + 97)  // a
+	expectedLabels.Set(k*256 + 101) // e
+	// And 'f' is prefix key
+	expectedIsPrefixKey.Set(k)
+
+	// Let's test it :)
+	assert.Nil(t, builder.Build(keys))
+
+	assert.True(t, expectedLabels.Equal(builder.Labels), "Expected Labels:\n%s\nGot:\n%s", expectedLabels, builder.Labels)
+	assert.True(t, expectedHasChild.Equal(builder.HasChild), "Expected HasChild:\n%s\nGot:\n%s", expectedHasChild, builder.HasChild)
+	assert.True(t, expectedIsPrefixKey.Equal(builder.IsPrefixKey), "Expected IsPrefixKey:\n%s\nGot:\n%s", expectedIsPrefixKey, builder.IsPrefixKey)
+
 }
